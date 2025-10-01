9d6eeada02543e282f355e2e7c3d00a8e7fb2cfb
Isfar Mahtab <153568201+isfar2@users.noreply.github.com>
2025-10-01 17:49:07 +0600

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,7 @@ export default {
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
+        sm: "calc(var(--radius) - 4px)", 
       },
       colors: {
         background: "var(--background)",
